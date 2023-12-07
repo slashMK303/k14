250c3038529a73157b5af617493041aacbb606ab
slashMK303 <slashluck3@gmail.com>
2023-12-07 07:57:24 +0700

    Update : k14-soup

--- k14-soup.py
+++ k14-soup.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd # analisi data
+import pandas as pd  # analisi data
 
 # url = "https://id.wikipedia.org/wiki/Windah_Basudara"
 url = input("Masukan url yang anda ingin analisis : ")
@@ -15,9 +15,14 @@ for header in soup.find_all(["h1", "h2", "h3", "h4", "h5", "h6"]):
     headers.append((text, level))
 
 df = pd.DataFrame(headers, columns=["Text", "Level"])
-df.index += 1 
+df.index += 1
 print(df)
 
-with open("output/output.txt", "w") as f:
+# mengambil nama untuk nama file dari akhir sampai bertemu "/"
+name = url.split("/")[-1]
+
+output_file = f"output/{name}.txt"
+
+with open(output_file, "w") as f:
     for index, row in df.iterrows():
         f.write(f"Text: {row['Text']}\nLevel: {row['Level']}\n\n")
