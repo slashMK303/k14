f2876bc27b6bd0445003b156d04c34ed9ea8db15
slashMK303 <slashluck3@gmail.com>
2023-12-13 17:46:12 +0700

    update : class, method

---
+++ k14-update-10.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+class WebAnalyzer:
+    def __init__(self, url):
+        self.url = url
+
+    def get_headers(self):
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        headers = []
+
+        for i, header in enumerate(soup.find_all(["h1", "h2", "h3", "h4", "h5", "h6"])):
+            text = header.text.strip()
+            level = header.name
+            headers.append((text, level))
+            if i == 9:
+                break
+
+        return headers
+
+    def save_to_file(self, headers, output_file):
+        with open(output_file, "w") as f:
+            for text, level in headers:
+                f.write(f"Text: {text}\nLevel: {level}\n\n")
+
+url = input("Masukkan URL yang ingin Anda analisis: ")
+analyzer = WebAnalyzer(url)
+headers = analyzer.get_headers()
+
+df = pd.DataFrame(headers, columns=["Text", "Level"])
+df.index += 1
+print(df)
+
+# mengambil nama untuk nama file dari akhir sampai bertemu "/"
+name = url.split("/")[-1]
+
+output_file = f"output/{name}.txt"
+analyzer.save_to_file(headers, output_file)
